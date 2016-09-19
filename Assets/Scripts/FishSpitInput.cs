59a3bfbc9809baf51e7643437e71fa76a69310f2
galhajaj <galhajaj@gmail.com>
2016-09-19 22:56:02 +0300

    continue working - end of day 1

---
+++ Assets/Scripts/FishSpitInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSpitInput : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.touchCount == 1)
+            Debug.Log("Touch!");
+
+        if (Input.touchCount == 2)
+        {
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+
+            DrawLine(Camera.main.ScreenToWorldPoint(touch1.position), Camera.main.ScreenToWorldPoint(touch2.position), Color.yellow, 0.1F);
+        }
+	}
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float width, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.SetColors(color, color);
+        lr.SetWidth(width, width);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+}
---
+++ Assets/Scripts/FlyGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyGenerator : MonoBehaviour 
+{
+    public GameObject FlyObject;
+    public float MaxTimeToGenerateFly;
+    public float MaxTimeToDestroyFly;
+
+    private float _timeToGenerateFly = 0.0F;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        _timeToGenerateFly -= Time.deltaTime;
+
+        if (_timeToGenerateFly <= 0.0F)
+        {
+            _timeToGenerateFly = Random.Range(0.0F, MaxTimeToGenerateFly);
+            spawnRandomFly();
+        }
+	}
+
+    private void spawnRandomFly()
+    {
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/2));
+        GameObject fly = Instantiate(FlyObject,screenPosition,Quaternion.identity) as GameObject;
+        Destroy(fly, Random.Range(0.0F, MaxTimeToDestroyFly));
+    }
+}
