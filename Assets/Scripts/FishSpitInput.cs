c8a3e072fbaaf0a4484345adee165e49dff157bd
galhajaj <galhajaj@gmail.com>
2016-09-22 00:25:30 +0300

    fix ray bug

--- Assets/Scripts/FishSpitInput.cs
+++ Assets/Scripts/FishSpitInput.cs
@@ -6,10 +6,11 @@ public class FishSpitInput : MonoBehaviour
 {
     public Text Test1Text;
     public Text Test2Text;
+
 	// Use this for initialization
 	void Start () 
     {
-	
+        
 	}
 	
 	// Update is called once per frame
@@ -30,15 +31,11 @@ public class FishSpitInput : MonoBehaviour
             Test2Text.text = Camera.main.ScreenToWorldPoint(new Vector3(touch2.position.x, touch2.position.y, 0.0F)).ToString();
 
             Vector3 vec1 = Camera.main.ScreenToWorldPoint(new Vector3(touch1.position.x, touch1.position.y, 0.0F));
-            vec1.z = 490.0F;
             Vector3 vec2 = Camera.main.ScreenToWorldPoint(new Vector3(touch2.position.x, touch2.position.y, 0.0F));
+            vec1.z = 490.0F;
             vec2.z = 490.0F;
 
-            //DrawLine(Camera.main.ScreenToWorldPoint(touch1.position), Camera.main.ScreenToWorldPoint(touch2.position), Color.yellow, 0.1F);
-            DrawLine(vec1, vec2, Color.yellow, 0.1F);
-            //DrawLine(touch1.position, touch2.position, Color.yellow, 0.1F);
-
-
+            DrawLine(vec1, vec2, Color.yellow, 0.05F);
         }
 	}
 
