88cd4afdaab9651dfd38d0f8f62014111d146b68
galhajaj <galhajaj@gmail.com>
2016-09-24 23:02:39 +0300

    finish - bad game....

---
+++ Assets/Scripts/MoveSpriteUpAndDown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSpriteUpAndDown : MonoBehaviour 
+{
+    public float DistanceToMove = 0.1F;
+    public float Speed = 0.5F;
+    private float _distanceMoved = 0.0F;
+    public bool IsGoUpFirst;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (IsGoUpFirst)
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + Speed * Time.deltaTime, this.transform.position.z);
+        else
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - Speed * Time.deltaTime, this.transform.position.z);
+
+        _distanceMoved += Time.deltaTime;
+
+        if (_distanceMoved >= DistanceToMove)
+        {
+            _distanceMoved = 0.0F;
+            IsGoUpFirst = !IsGoUpFirst;
+        }
+	}
+}
